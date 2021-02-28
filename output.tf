6a76fc70bf7dd3f9a9ae9622e83c86b9de57a9c6
Ludovic Logiou <ludovic.logiou@gmail.com>
2021-02-28 19:44:54 +0100

    Add more outputs

--- output.tf
+++ output.tf
@@ -1,8 +1,31 @@
+// k8s instance outputs
+output "kapsule_id" {
+  value       = scaleway_k8s_cluster.this.id
+  description = "Kapsule instance ID"
+
+}
+
+output "kapsule_wildcard_dns" {
+  value       = scaleway_k8s_cluster.this.wildcard_dns
+  description = "DNS wildcard that points to all ready nodes"
+}
+
 output "apiserver_url" {
-  value     = scaleway_k8s_cluster.this.apiserver_url
+  value = scaleway_k8s_cluster.this.apiserver_url
 }
 
 output "kubeconfig" {
-  value = scaleway_k8s_cluster.this.kubeconfig[0]
+  value     = scaleway_k8s_cluster.this.kubeconfig[0]
   sensitive = true
+}
+
+output "kapsule_upgrade_available" {
+  value       = scaleway_k8s_cluster.this.upgrade_available
+  description = "Set to true if a newer Kubernetes version is available"
+}
+
+// k8s nodepool outputs
+
+output "nodepools" {
+  value = scaleway_k8s_pool.this
 }
